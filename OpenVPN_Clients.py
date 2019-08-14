d4fb2435f688c7f4d177f04bb34aeacf176c1343
James Kirsop <james.kirsop@gmail.com>
2019-08-14 15:03:57 +1000

    Cleaning up after initial commit

--- OpenVPN_Clients.py
+++ OpenVPN_Clients.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-conf_dir = ''
+conf_dir = '/etc/openvpn/nms/client-configs'
 
 import telnetlib
 import re
@@ -19,7 +19,6 @@ con.expect([re.compile(b",\d{10,}\\r\\n",)])
 con.expect([b'HEADER,'])
 first = con.read_until(b'HEADER,')
 for row in csv.DictReader(first.decode('ascii').rstrip('\r\nHEADER,').split("\r\n")):
-    # print(row)
     returnList.append({
         "{#HOSTNAME}" : row['Common Name'],
         "{#VPNv4Address}" : row['Virtual Address'],
@@ -38,36 +37,3 @@ con.write(b"exit\n")
 res = con.read_all()
 con.close()
 print(json.dumps({"data":returnList},indent=4))
-
-# print('========')
-# print(res.decode('ascii'))
-# print(listConfigs)
-
-# re.search(b",\d{10,}\\r\\n",b"08:29:45 2019,1565735385\r\n")
-
-# re.search("[A-z]","aaa")
-# con = telnetlib.Telnet('localhost',7505)
-# con.read_lazy()
-# time.sleep(.1)
-# con.write(b"status 2\n")
-# con.write(b"exit\n")
-# res = con.read_all()
-# print(res)
-# print(listdir(conf_dir))
-# print(len(listdir(conf_dir)))
-
-# tnc = telnetlib.Telnet
-# conn = telnetlib.Telnet('localhost',7505)
-# conn.set_debuglevel(1)
-# conn.read_lazy()
-# conn.write(b"status 2\n")
-# conn.write(b"exit\n\n")
-# print(conn.read_until(b'END\r\n',timeout=2).decode('ascii'))
-# conn.close()
-
-# conn.read_until(b"INFO:",timeout=2)
-# conn.open()
-# conn.read_until(b">INFO:OpenVPN Management Interface Version 1 -- type 'help' for more info")
-
-#con.read_until("'help' for more info")
-# con.read_lazy()
